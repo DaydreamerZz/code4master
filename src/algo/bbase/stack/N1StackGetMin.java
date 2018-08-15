739072a4fa89e24ea0b772b98889e0fd255ba9ba
zz <zhzh402@163.com>
2018-08-15 20:04:41 +0800

    1

---
+++ src/algo/bbase/stack/N1StackGetMin.java
@@ -0,0 +1,36 @@
+package algo.bbase.stack;
+
+import java.util.Stack;
+
+/**
+ * @author : Bruce Zhao
+ * @email : zhzh402@163.com
+ * @date : 2018/8/15 19:59
+ * @desc :
+ */
+public class N1StackGetMin {
+
+    public static void main(String[] args) {
+
+        return;
+    }
+}
+
+/*
+ * 3 4 5 1 2 1
+ * 3 - - 1 - 1
+ *
+ */
+class MyStack1{
+    Stack<Integer> dataStack = new Stack<>();
+    Stack<Integer> miniStack = new Stack<>();
+
+    public void push(int value){
+        if(miniStack.isEmpty()){
+            dataStack.push()
+        }
+    }
+    public int pop(){
+
+    }
+}
