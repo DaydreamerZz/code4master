700317c00efe8b48497d3e647eaeb78a54bac1f4
zhao surface <zhzh402@163.com>
2018-08-25 12:05:17 +0800

    cnm bytedance

--- src/codeexam/bytedance/second/Main1.java
+++ src/codeexam/bytedance/second/Main1.java
@@ -1,15 +1,139 @@
 package codeexam.bytedance.second;
 
+import sun.awt.image.ImageWatched;
+
+import java.util.*;
+
 /**
  * @author : Bruce Zhao
  * @email : zhzh402@163.com
  * @date : 2018/8/25 9:38
  * @desc :
+10
+0
+5 3 0
+8 4 0
+9 0
+9 0
+3 0
+0
+7 9 0
+0
+9 7 0
  */
 public class Main1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int len = Integer.parseInt(scanner.nextLine());
+        Map<Integer, LinkedList<Integer>> map = new HashMap<>();
+        boolean[] isVisited = new boolean[len+1];
+        for(int i = 0; i < len; i++){
+            String line = scanner.nextLine();
+            String[] split = line.split(" ");
+            LinkedList<Integer> curLink = new LinkedList<>();
+            for(String tmp : split){
+                int tmpValue = Integer.parseInt(tmp);
+                if(tmpValue != 0)
+                    curLink.add(tmpValue);
+                else
+                    break;
+            }
+            map.put(i+1, curLink);
+        }
+        Map<Integer, LinkedList<Integer>> resMap = new HashMap<>();
 
+        Iterator<Map.Entry<Integer, LinkedList<Integer>>> iterator = map.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<Integer, LinkedList<Integer>> next = iterator.next();
+            int key = next.getKey();
+
+            if(isVisited[key]){
+                continue;
+            }
+            LinkedList<Integer> valueList = next.getValue();
+            if(valueList.isEmpty()){
+                resMap.put(key, null);
+                isVisited[key] = true;
+            }else{
+                LinkedList<Integer> tmpList = resMap.get(key);
+                if(tmpList == null){
+                    tmpList = new LinkedList<>();
+                }
+                isVisited[key] = true;
+                Queue<Integer> queue = new ArrayDeque<>();
+                for(int i = 0; i < valueList.size(); i++){
+                    queue.add(valueList.get(i));
+                }
+                while(!queue.isEmpty()){
+                    Integer poll = queue.poll();
+                    tmpList.add(poll);
+
+                    LinkedList<Integer> integers = map.get(poll);
+                    if(isVisited[poll])
+                        continue;
+                    else {
+                        isVisited[poll] = true;
+                        for (Integer x : integers) {
+                            queue.add(x);
+                        }
+                    }
+                }
+                resMap.put(key, tmpList);
+            }
+
+        }
+
+        Iterator<Map.Entry<Integer, LinkedList<Integer>>> iterator2 = resMap.entrySet().iterator();
+        ArrayList<LinkedList<Integer>> list = new ArrayList<>();
+        while(iterator2.hasNext()){
+            Map.Entry<Integer, LinkedList<Integer>> next = iterator2.next();
+            Integer key = next.getKey();
+            LinkedList<Integer> valueList = next.getValue();
+            list.add(valueList);
+
+        }
+        int count = list.size();
+        for(int i = 0; i < list.size(); i++){
+            LinkedList<Integer> integers = list.get(i);
+            if(integers == null){
+                continue;
+            }
+            for(int j = 0; j < list.size(); j++){
+                LinkedList<Integer> integers1 = list.get(j);
+                if(integers1 == null)
+                    continue;
+                if(j == i)
+                    continue;
+                boolean flag = true;
+                for(Integer tmp : integers){
+                    if(!integers1.contains(tmp)){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    count--;
+                }
+            }
+        }
+
+//        Iterator<Map.Entry<Integer, LinkedList<Integer>>> iterator3 = resMap.entrySet().iterator();
+//        while(iterator3.hasNext()){
+//            Map.Entry<Integer, LinkedList<Integer>> next = iterator3.next();
+//            System.out.println(next.getKey() + " " +next.getValue());
+//        }
+
+
+        /*Iterator<Map.Entry<Integer, LinkedList<Integer>>> iterator = map.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<Integer, LinkedList<Integer>> next = iterator.next();
+            System.out.println(next.getKey() + " " +next.getValue());
+        }*/
+
+        System.out.println(count);
+//        System.out.println(resMap.size());
         return;
     }
 }
+
--- src/codeexam/bytedance/second/Main3.java
+++ src/codeexam/bytedance/second/Main3.java
@@ -1,15 +1,82 @@
 package codeexam.bytedance.second;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @author : Bruce Zhao
  * @email : zhzh402@163.com
  * @date : 2018/8/25 9:38
  * @desc :
+1
+2
+helloworld
+hdlrowolle
+1
+2
+abcde
+acbde
  */
 public class Main3 {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int len = Integer.parseInt(scanner.nextLine());
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i < len; i++){
+            int tmpLen = Integer.parseInt(scanner.nextLine());
+            for(int j = 0; j < tmpLen; j++) {
+                list.add(scanner.nextLine());
+            }
+            System.out.println(process(list));
+        }
         return;
+    }
+
+    private static String process(ArrayList<String> list) {
+        String res = "Sad";
+        String s = list.get(0);
+        s = s + s;
+        char[] chars1 = s.toCharArray();
+        for(int i = 1; i < list.size(); i++){
+            String s1 = list.get(i);
+            char[] arr1 = s1.toCharArray();
+            StringBuilder sb = new StringBuilder(s1);
+            sb.reverse();
+            char[] arr2 = sb.toString().toCharArray();
+            boolean check1 = check(chars1, arr1);
+            if(check1)
+                return "Yeah";
+            boolean check2 = check(chars1, arr2);
+            if(check2)
+                return "Yeah";
+
+        }
+
+        return "Sad";
+    }
+
+    private static boolean check(char[] chars1, char[] arr2) {
+        for(int i = 0; i < chars1.length; i++){
+            boolean flag = true;
+            int index = i;
+            for(int j = 0; j < arr2.length; j++){
+                if(index >= chars1.length){
+                    flag = false;
+                    break;
+                }
+
+                if(chars1[index] != arr2[j]){
+                    flag = false;
+                    break;
+                }else{
+                    index++;
+                }
+            }
+            if(flag)
+                return true;
+        }
+        return false;
     }
 }
