577f5422692ca71b8279245bc768b441014a15e4
DaydreamerZZ <zhzh402@163.com>
2018-08-31 11:56:24 +0800

    dp

--- src/algo/bbase/dp/N5LongestIncSeq.java
+++ src/algo/bbase/dp/N5LongestIncSeq.java
@@ -1,5 +1,7 @@
 package algo.bbase.dp;
 
+import java.util.Arrays;
+
 /**
  * @author : Bruce Zhao
  * @email : zhzh402@163.com
@@ -12,19 +14,56 @@ public class N5LongestIncSeq {
 
         N5LongestIncSeq longestIncSeq = new N5LongestIncSeq();
         int[] arr = new int[]{2, 1, 5, 3, 6, 4, 8, 9, 7};
-        longestIncSeq.getLIS(arr);
-
+        int[] lis = longestIncSeq.getLIS(arr);
+        System.out.println(Arrays.toString(lis));
 
         return;
     }
 
-    private void getLIS(int[] arr) {
-        int[] dp = getdp(arr);
+    private int[] getLIS(int[] arr) {
+        int[] dp = getdp(arr); //拿到每个位置的最长递增序列长度
+        int[] lis = generateLIS(arr, dp); //利用每个位置的长度推导出递增序列
+        return lis;
+    }
+
+    private int[] generateLIS(int[] arr, int[] dp) {
+        int len = 0;
+        int index = 0;
+        for (int i = 1; i < dp.length; i++) {
+            if (dp[i] > len) {
+                len = dp[i];
+                index = i;
+            }
+        }
+
+        int[] res = new int[len];
+        res[--len] = arr[index];
+
+        for(int i = index; i >=0; i--){
+            if(arr[i] < arr[index] && dp[i] == dp[index]-1){
+                res[--len] = arr[i];
+                index = i;
+            }
+        }
+
+        return res;
     }
 
     private int[] getdp(int[] arr) {
         int[] dp = new int[arr.length];
+        dp[0] = 1;
+        int max = 0;
+        for(int i = 1; i < arr.length; i++){
+            max = 0;
+            for(int j = 0; j < i; j++){
+                if(arr[j] < arr[i] && dp[j] > max){
+                    max = dp[j];
+                }
+            }
+            dp[i] = max + 1;
+        }
 
+        System.out.println(Arrays.toString(dp));
         return dp;
     }
 }
